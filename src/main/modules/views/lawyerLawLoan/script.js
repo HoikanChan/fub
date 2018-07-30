91a5964b50d48cffc16345ec42ddea30e43581ed
chenhq <chenhq@rsclouds.com>
2018-07-30 17:19:05 +0800

    律师中心 法务贷

--- src/main/modules/views/lawyerLawLoan/script.js
+++ src/main/modules/views/lawyerLawLoan/script.js
@@ -1,56 +1,68 @@
 var userCenter = (function() {
   var page = 1
   var inquery_validate
-
-  function getMycase() {
+  var listData
+  var start = {
+    isinitVal: true,
+    initDate: [{ DD: '-7' }, true],
+    format: 'YYYY-MM-DD',
+    maxDate: $.nowDate({ DD: 0 }), //最大日期
+    zIndex: 99999,
+    isClear: false,
+    isok: false,
+    okfun: function(elem, date) {
+      end.minDate = elem.val.replace(/\//g, '-') //开始日选好后，重置结束日的最小日期
+      //   endDates();
+    }
+  }
+  var end = {
+    isinitVal: true,
+    isok: false,
+    isClear: false,
+    zIndex: 99999,
+    maxDate: $.nowDate({ DD: 0 }), //最大日期
+    format: 'YYYY-MM-DD',
+    okfun: function(elem, date) {
+      start.maxDate = elem.val.replace(/\//g, '-') //将结束日的初始值设定为开始日的最大日期
+    }
+  }
+  function getMyloan() {
     var params = {
-      clientId: clientId,
+      lawyerId: lawyerId,
       sidx: 'createTime',
       order: 'desc'
     }
 
-    $('#my-cases .pager').tablePager({
-      url: 'order/queryOrderList',
+    $('#my-loans .pager').tablePager({
+      url: 'loan/queryLawyerLoanOrderList',
       searchParam: params,
       success: function(result) {
         if (result.code == 0) {
           if (result) {
-            var html = template('case-result-templete', result.data)
-
-            $('.my-case-box').html(html)
+            var html = template('loan-result-templete', result.data)
+            listData = result.data.list
+            $('.totalNum').html(result.data.totalCount)
+            $('.my-loan-box').html(html)
+            if (result.data.totalCount < 10) {
+              $('.page-row').hide()
+            } else {
+              $('.page-row').show()
+            }
+            if (result.data.totalCount == 0) {
+              $('.my-loans-box').html(
+                "<P class='noresult'>抱歉，没有相关案件</P>"
+              )
+            }
+          } else {
+            toastr.warning(result.msg)
           }
+          $('.my-loans-content .totalNum').text(result.data.totalCount)
         }
       }
     })
-
-    //   $('#my-cases .mycasepage .pager').tablePager({
-    //     url: "order/queryOrderList",
-    //     searchParam:params,
-    //     success: function (result) {
-    //        if(result.code == 0){
-    //          console.log(result.data)
-    //           // var html1 = template('my-cases-template', result.data);
-    //           // $(".my-cases-box").html(html1);
-    //           var html = template('search-result-templete', result.data);
-    //           $(".my-case-box").html(html);
-
-    //         if(result.data.totalCount<10){
-    //             $(".page-row").hide()
-    //         }else{
-    //             $(".page-row").show()
-    //         }
-    //         if(result.data.totalCount==0){
-    //             $(".my-cases-box").html("<P class='noresult'>抱歉，没有相关案件</P>")
-    //         }
-    //        }else{
-    //             toastr.warning(result.msg);
-    //        }
-    //       $(".my-cases-content .totalNum").text(result.data.totalCount);
-    //     }
-    // })
   }
   $(function() {
-
+    $('#lawyer-loan').addClass('active')
     $('aside .right-icon').click(function(e) {
       e.stopPropagation()
       if (e.target.classList.contains('fa-chevron-down')) {
@@ -69,40 +81,52 @@ var userCenter = (function() {
           .addClass('fa-chevron-down')
       }
     })
+    //时间选择
+    $('#start-date').jeDate(start)
+    $('#end-date').jeDate(end)
+    $('.all-status').on('change', function() {
+      var select = $("select[name='status']")
+        .find('option:selected')
+        .val()
+    })
   })
   return {
     init: function() {
-      $('.lawyer-loan-content').html(
-        template('lawyer-loan-template', {
-          loans: {},
-          bills: [
-            {
-              no: '1123234256532',
-              amount: '250.00',
-              lawyer: '仗义飞',
-              phone: '12345671223',
-              state: '平台审批中',
-              time: '2015-06-05 15:33:30'
-            },
-            {
-              no: '1123234256532',
-              amount: '250.00',
-              lawyer: '仗义飞',
-              phone: '12345671223',
-              state: '平台审批中',
-              time: '2015-06-05 15:33:30'
-            },
-            {
-              no: '1123234256532',
-              amount: '250.00',
-              lawyer: '仗义飞',
-              phone: '12345671223',
-              state: '平台审批中',
-              time: '2015-06-05 15:33:30'
+      getMyloan()
+      $(document).on('click', '.showmore', function() {
+        var loanid = $(this).attr('data-id')
+        // 根据loanid在loan列表查找loanStatus
+        var loanStatus = listData.filter(function(loan) {
+          return Number(loanid) === Number(loan.id)
+        })[0].loanStatus
+        $({ loanId: loanid })._Ajax({
+          url: 'loanauditlog/AuditLogByLoanId',
+          success: function(result) {
+            var html = []
+            if (result.code == 0) {
+              var data = result.list
+              for (var i = 0; i < data.length; i++) {
+                html +=
+                  "<div class='pro-tex'>" +
+                  data[i].desc +
+                  '<span>' +
+                  data[i].createTime +
+                  '</span></div>'
+              }
+              $('#tr' + loanid + ' .steps-box .step').each(function(index) {
+                if (index < loanStatus + 1) {
+                  $(this).addClass('active-step')
+                }
+              })
+              $('#tr' + loanid + ' .process-text').html(html)
+              $('#tr' + loanid).toggle()
             }
-          ]
+          }
         })
-      )
+      })
+      $(document).on('click', '.check-file-btn', function() {
+        downloadModal.showModal($(this).attr('data-id'))
+      })
     }
   }
 })()
